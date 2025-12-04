d8dcfff412d58db763c9dff72159a61f6ddc3bcc
Joseph Spearritt <joe.spearritt@live.com>
2025-12-04 21:12:13 +1100

    Day 3 part 2

--- src/main/scala/aoc25/day3/Day3.scala
+++ src/main/scala/aoc25/day3/Day3.scala
@@ -1,14 +1,20 @@
 package aoc25.day3
 
-
 import aoc25.common.{CommonParsers, SolutionWithParser}
 import cats.parse.{Numbers, Parser}
 
-import scala.util.control.Breaks._
-import scala.collection.mutable.ArrayBuffer
+def seqToJoltage(l: Seq[Int]): Long =
+  l.mkString("").toLong
 
-def seqToJoltage(l: Seq[Int]): Int =
-  l.mkString("").toInt
+def getMaxJoltage(l: Seq[Int], count: Int): Long =
+  var best: List[Int] = List()
+  for digit <- l do
+    if best.size < count then
+      best = best.appended(digit)
+    else
+      val potentials = (0 until count).map(i => best.take(i) ++ best.drop(i+1) ++ List(digit)).toList
+      best = (best :: potentials).maxBy(seqToJoltage(_))
+  seqToJoltage(best)
 
 
 /**
@@ -16,25 +22,16 @@ def seqToJoltage(l: Seq[Int]): Int =
  *
  * The parts are counting the number of elements in the list, and summing them
  */
-object Day3 extends SolutionWithParser[List[List[Int]], Int, Int]:
+object Day3 extends SolutionWithParser[List[List[Int]], Long, Long]:
   override def dayNumber: Int = 3
 
   override def parser: Parser[List[List[Int]]] = CommonParsers.grid(Numbers.digit.map(_.asDigit))
 
-  override def solvePart1(input: List[List[Int]]): Int =
-    input.map { row =>
-      var best: List[Int] = List()
-      for digit <- row do
-        if best.size < 2 then
-          best = best.appended(digit)
-        else
-          val potentialA = best.drop(1).appended(digit)
-          val potentialB = best.take(1).appended(digit)
-          best = List(best, potentialA, potentialB).maxBy(seqToJoltage(_))
-      best
-    }.map(seqToJoltage(_)).sum
+  override def solvePart1(input: List[List[Int]]): Long =
+    input.map(getMaxJoltage(_, 2)).sum
 
-  override def solvePart2(input: List[List[Int]]): Int = 0
+  override def solvePart2(input: List[List[Int]]): Long =
+    input.map(getMaxJoltage(_, 12)).sum
 
 
 @main def run(): Unit = Day3.run()
